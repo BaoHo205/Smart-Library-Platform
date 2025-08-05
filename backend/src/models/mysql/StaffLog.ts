2d2ed0350a54e1c60a9324e6852c80669f7efea5
Diem Qui <huynhlediemqui.vn@gmail.com>
2025-08-05 13:51:16 +0700

    feat: add database schema for both sql and nosql [QuiHLD]

---
+++ backend/src/models/mysql/StaffLog.ts
@@ -0,0 +1,11 @@
+import { ActionType } from './enum/ActionType';
+
+export interface StaffLog {
+  id?: string;
+  userId: string;
+  bookId: string;
+  actionType: ActionType;
+  actionDetails: string;
+  createdAt: Date;
+  updatedAt: Date;
+ }
