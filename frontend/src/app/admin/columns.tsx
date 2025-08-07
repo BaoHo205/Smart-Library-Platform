44f9d984d47f6722a20eeb6d1b18872cbd7972b4
TanNguyen17 <s4053063@rmit.edu.vn>
2025-08-08 00:12:31 +0700

    create table ui

---
+++ frontend/src/app/admin/columns.tsx
@@ -0,0 +1,68 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { ColumnDef } from "@tanstack/react-table"
+import Image from "next/image"
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type BookShow = {
+    thumbnail_url: string
+    id: string
+    title: string
+    author: string
+    publisher: string
+    genre: [string]
+    quantity: number
+}
+
+export const columns: ColumnDef<BookShow>[] = [
+    {
+        accessorKey: "thumbnail_url",
+        header: "Book Image",
+        cell: ({ row }) => {
+            const url = row.getValue("thumbnail_url") as string;
+            return (
+                <img
+                    src={url}
+                    alt="Book Thumbnail"
+                    className="h-16 w-auto rounded-md object-cover"
+                />
+            );
+        },
+    },
+    {
+        accessorKey: "id",
+        header: "Book ID",
+    },
+    {
+        accessorKey: "title",
+        header: "Book Name",
+    },
+    {
+        accessorKey: "author",
+        header: "Book Author",
+    },
+    {
+        accessorKey: "publisher",
+        header: "Publisher",
+    },
+    {
+        accessorKey: "genre",
+        header: "Genre",
+    },
+    {
+        accessorKey: "quantity",
+        header: "Quantity",
+    },
+    {
+        id: "edit",
+        cell: ({ row }) => {
+            return (
+                <Button>
+                    Edit
+                </Button>
+            )
+        }
+    }
+]
