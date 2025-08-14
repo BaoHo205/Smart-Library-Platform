e09c9f0fe75779586f8a03a08773264752c9fa76
Jackydo <124519045+JackyDo2005@users.noreply.github.com>
2025-08-15 01:45:26 +0700

    feat: update image cover (veri cute)

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -22,8 +22,8 @@ export default function LoginPage() {
       </div>
       <div className="bg-muted relative hidden lg:block">
         <img
-          src="/placeholder.svg"
-          alt="Image"
+          src="/DB_cover_image.png"
+          alt="5aesiunhan cam sach"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
       </div>
