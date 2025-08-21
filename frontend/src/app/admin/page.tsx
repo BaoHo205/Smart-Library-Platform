f00169b04be015aa74a9c15ee322d696d6255203
TanNguyen17 <s4053063@rmit.edu.vn>
2025-08-21 12:17:29 +0700

    add use data store for book

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -12,55 +12,26 @@ import axiosInstance from '@/config/axiosConfig';
 import { useDataStore } from '@/lib/useDataStore';
 
 const page = () => {
-    const [books, setBooks] = useState<BookShow[]>([]);
-    // const token = localStorage.getItem("accessToken");
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIwNzFmNDY3Yy0wNzVjLTQ4ZjktYjc2Yi1lNTYwYTkxZDhlMGMiLCJyb2xlIjoidXNlciIsInR5cGUiOiJhY2Nlc3MiLCJpYXQiOjE3NTUwOTc4MDQsImV4cCI6MTc1NTA5OTYwNH0.higNS8V7qKn5ZiWFKxt0NSOM-IlpUD4d3xfS0PtnlfA";
+    const setBooks = useDataStore((s) => s.setBooks);
     const setPublishers = useDataStore((s) => s.setPublishers);
     const setAuthors = useDataStore((s) => s.setAuthors);
     const setGenres = useDataStore((s) => s.setGenres);
+    const books = useDataStore((s) => s.books);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const bookResponse = await axiosInstance.get('/api/v1/books',
-                    {
-                        headers: {
-                            Authorization: `Bearer ${token}`
-                        },
-                        withCredentials: true
-                    }
-                );
+                const bookResponse = await axiosInstance.get('/api/v1/books');
                 setBooks(bookResponse.data.result.data);
 
-                const authorResponse = await axiosInstance.get('/api/v1/authors',
-                    {
-                        headers: {
-                            Authorization: `Bearer ${token}`
-                        },
-                        withCredentials: true
-                    }
-                );
+                const authorResponse = await axiosInstance.get('/api/v1/authors');
                 setAuthors(authorResponse.data.data);
 
-                const publisherResponse = await axiosInstance.get('/api/v1/publishers',
-                    {
-                        headers: {
-                            Authorization: `Bearer ${token}`
-                        },
-                        withCredentials: true
-                    }
-                );
+                const publisherResponse = await axiosInstance.get('/api/v1/publishers');
 
                 setPublishers(publisherResponse.data.data);
 
-                const genreResponse = await axiosInstance.get('/api/v1/genres',
-                    {
-                        headers: {
-                            Authorization: `Bearer ${token}`
-                        },
-                        withCredentials: true
-                    }
-                );
+                const genreResponse = await axiosInstance.get('/api/v1/genres');
                 setGenres(genreResponse.data.data);
             } catch (error) {
                 console.error('Error fetching data:', error);
--- frontend/src/lib/useDataStore.ts
+++ frontend/src/lib/useDataStore.ts
@@ -1,3 +1,4 @@
+import { BookShow } from "@/app/admin/columns";
 import { create } from "zustand";
 
 export interface Publisher {
@@ -20,17 +21,23 @@ type DataState = {
     publisherList: Publisher[];
     authorList: Author[];
     genreList: Genre[];
+    books: BookShow[];
     addPublisher: (pub: Publisher) => void;
     addAuthor: (auth: Author) => void;
+    addBook: (book: BookShow) => void;
     setPublishers: (list: Publisher[]) => void;
     setAuthors: (list: Author[]) => void;
     setGenres: (list: Genre[]) => void;
+    setBooks: (list: BookShow[]) => void;
+    retireBook: (bookId: string) => void;
+    updateQuantity: (bookId: string, newQuantity: number) => void;
 };
 
 export const useDataStore = create<DataState>((set) => ({
     publisherList: [],
     authorList: [],
     genreList: [],
+    books: [],
 
     addPublisher: (pub) =>
         set((state) => ({ publisherList: [...state.publisherList, pub] })),
@@ -38,10 +45,37 @@ export const useDataStore = create<DataState>((set) => ({
     addAuthor: (auth) =>
         set((state) => ({ authorList: [...state.authorList, auth] })),
 
+    addBook: (book) =>
+        set((state) => ({ books: [...state.books, book] })),
+
     setPublishers: (list) => set(() => ({ publisherList: list })),
     setAuthors: (list) => set(() => ({ authorList: list })),
+    setGenres: (list) => set(() => ({ genreList: list })),
+    setBooks: (list) => set(() => ({ books: list })),
 
-    setGenres: (list) => set(() => ({ genreList: list }))
+    retireBook: (bookId) =>
+        set((state) => ({
+            books: state.books.map((book) =>
+                // Find the book with the matching ID
+                book.id === bookId
+                    ? // If found, return a new object with the `isRetired` property updated
+                    { ...book, status: "unavailable" }
+                    : // Otherwise, return the book unchanged
+                    book
+            ),
+        })),
+
+    updateQuantity: (bookId, newQuantity) =>
+        set((state) => ({
+            books: state.books.map((book) =>
+                // Find the book with the matching ID
+                book.id === bookId
+                    ? // If found, return a new object with the `isRetired` property updated
+                    { ...book, quantity: newQuantity }
+                    : // Otherwise, return the book unchanged
+                    book
+            ),
+        })),
 }));
 
 
